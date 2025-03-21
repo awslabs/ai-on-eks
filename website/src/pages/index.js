280f3a07f08de736c4783229f13a859f2ff35586
omrishiv <327609+omrishiv@users.noreply.github.com>
2025-03-21 16:53:47 -0700

    initial commit
    Signed-off-by: omrishiv <327609+omrishiv@users.noreply.github.com>

---
+++ website/src/pages/index.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import clsx from 'clsx';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
+import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import VideoGrid from '@site/src/components/VideoGrid/VideoGrid';
+import styles from './index.module.css';
+
+function HomepageHeader() {
+  const { siteConfig } = useDocusaurusContext();
+
+  return (
+      <header className={clsx('hero hero--primary', styles.heroBanner)}>
+          <video autoPlay muted loop id="heroVideo">
+              <source src="vid/background.mp4" type="video/mp4"/>
+          </video>
+              <p
+                  className='hero__subtitle'
+                  style={{
+                      fontSize: 18,
+                      fontSmooth: 'auto',
+                  }}>
+                  {siteConfig.tagline}
+              </p>
+      </header>
+);
+}
+
+export default function Home() {
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <Layout
+      title={`Hello from ${siteConfig.title}`}
+      description="Description will go into a meta tag in <head />">
+      <HomepageHeader />
+      <main>
+        <div className="container">
+          <HomepageFeatures />
+        </div>
+      </main>
+    </Layout>
+  );
+}
